04cb3a89c7774c2dd32bc63e9d54ce8e092ca93e
NAVEENKUMAR S <80678046+NaveenKumar-n4v@users.noreply.github.com>
2022-01-30 22:40:16 +0530

    Time: 533 ms (36.36%), Space: 286.6 MB (22.73%) - LeetHub

--- 2155-all-divisions-with-the-highest-score-of-a-binary-array/2155-all-divisions-with-the-highest-score-of-a-binary-array.cpp
+++ 2155-all-divisions-with-the-highest-score-of-a-binary-array/2155-all-divisions-with-the-highest-score-of-a-binary-array.cpp
@@ -1,38 +1,27 @@
 class Solution {
 public:
-    vector<int> maxScoreIndices(vector<int>& nums) {
-        int left=0;
-        int n =nums.size();
-        int x=0;
-        vector<pair<int,int>>v;
-        for(int i=0;i<n;i++){
-            if(nums[i]==0){
-                left++;
-            }
-            if(nums[i]==1){
-                x++;
-            }
+    vector<int> maxScoreIndices(vector<int>& nums) {    
+        int right=0;
+        for(auto i : nums){
+        right+=i;
         }
-        v.push_back({x,0});
-        left=0;
-        for(int i=0;i<n;i++){
-            if(nums[i]==0){
-                left++;
+        int mx = right;
+        int left =0;
+        vector<int>ans;
+        for(int i=0;i<=nums.size();i++){
+            if(left + right > mx){
+                mx = left + right;
+                ans.clear();
             }
-            if(nums[i]==1){
-                x--;
+            if(left + right == mx){
+                ans.push_back(i);
             }
-            v.push_back({left+x,i+1});
-        }
-        sort(v.begin(),v.end(),greater<pair<int,int>>());
-        vector<int>v1;
-        int fk =v[0].first;
-            int i=0;
-      while(v[i].first==fk && i<n){
-         v1.push_back(v[i].second);
-          i++;
-      }
-        return v1;
+            if(i<nums.size()){
+                right -= nums[i];
+                left +=(nums[i]==0);
+            }
+}
+        return ans;
         
     }
 };
