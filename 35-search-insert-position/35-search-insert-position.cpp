62cce9eb5553c65d5df68048293a13a917421abc
NAVEENKUMAR S <80678046+NaveenKumar-n4v@users.noreply.github.com>
2022-02-07 21:52:46 +0530

    Time: 4 ms (82.18%), Space: 9.8 MB (22.68%) - LeetHub

--- 35-search-insert-position/35-search-insert-position.cpp
+++ 35-search-insert-position/35-search-insert-position.cpp
@@ -1,11 +1,6 @@
 class Solution {
 public:
     int searchInsert(vector<int>& nums, int target) {
-        int val = lower_bound(nums.begin(),nums.end(),target)-nums.begin();
-        if(nums[val]==target){
-            return val;
-        }
-        else
-            return val;
+        return lower_bound(nums.begin(),nums.end(),target)-nums.begin();
     }
 };
