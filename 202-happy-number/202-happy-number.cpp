2f76fd1e71d49d7fd56f60cdc7bac85aab28c0d4
NAVEENKUMAR S <80678046+NaveenKumar-n4v@users.noreply.github.com>
2022-02-08 22:12:47 +0530

    Time: 2 ms (55.70%), Space: 5.8 MB (94.68%) - LeetHub

--- 202-happy-number/202-happy-number.cpp
+++ 202-happy-number/202-happy-number.cpp
@@ -1,22 +1,43 @@
+// class Solution {
+// public:
+//     bool isHappy(int n) {
+        
+//         int count = 0;
+//        while(n != 1)
+//         {
+//             int sum = 0;
+//             while(n != 0)
+//             {
+//                 int num = n%10;
+//                 sum += num*num;
+//                 n/=10;
+//                 count++;
+//             }
+//             n = sum;
+//             if(count > 31)
+//                 return false;
+//         }
+//         return true;
+//     }
+// };
 class Solution {
 public:
     bool isHappy(int n) {
-        
-        int count = 0;
-       while(n != 1)
-        {
-            int sum = 0;
-            while(n != 0)
-            {
-                int num = n%10;
-                sum += num*num;
+        int count =0;
+        while(n>1){
+            int val =0;
+            while(n){
+                int digit =n%10;
+                val += digit*digit;
                 n/=10;
                 count++;
             }
-            n = sum;
-            if(count > 31)
+            n = val;
+            if(count>31){
                 return false;
+            }
         }
-        return true;
+     return true;
+        
     }
 };
