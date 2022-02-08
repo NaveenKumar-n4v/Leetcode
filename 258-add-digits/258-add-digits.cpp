544bdd36e5b95aba1f290303592131dc158b8a66
NAVEENKUMAR S <80678046+NaveenKumar-n4v@users.noreply.github.com>
2022-02-08 21:51:09 +0530

    Time: 4 ms (43.34%), Space: 5.8 MB (95.57%) - LeetHub

--- 258-add-digits/258-add-digits.cpp
+++ 258-add-digits/258-add-digits.cpp
@@ -1,7 +1,17 @@
 class Solution {
 public:
     int addDigits(int num) {
-    //Really An Amazing Problem..
-    return 1 + (num - 1)%9;
+    int val=0;
+    while(num>9)
+    {
+        int digit =0;
+        while(num>0){
+            digit += num%10;
+            num /=10;
+        }
+        num = digit;
+        val=0;
+    }
+        return num;
     }
 };
