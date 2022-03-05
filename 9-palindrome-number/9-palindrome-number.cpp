663cd8d9cec5815be0f6facef11efc7d40b69afa
NAVEENKUMAR S <80678046+NaveenKumar-n4v@users.noreply.github.com>
2022-03-05 21:28:38 +0530

    Time: 24 ms (38.63%), Space: 6 MB (32.96%) - LeetHub

--- 9-palindrome-number/9-palindrome-number.cpp
+++ 9-palindrome-number/9-palindrome-number.cpp
@@ -1,11 +1,19 @@
-class Solution {
+class Solution 
+{
 public:
-    bool isPalindrome(int x) {
-        string s = to_string(x);
-        if(s == string(s.rbegin(), s.rend())) {
-            return true;
-        } else {
-            return false;
-        }
+    bool isPalindrome(int x) 
+    {
+       long int digit,reverse=0;
+       int temp=x;
+       while(x>0)
+       {
+           digit=x%10;
+           reverse=(reverse*10)+digit;
+           x=x/10;
+       }
+      if(temp==reverse)
+      {return true;}
+      else
+      {return false;}  
     }
 };
