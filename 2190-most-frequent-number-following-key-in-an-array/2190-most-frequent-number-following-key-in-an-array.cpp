0024a1b071d547310596a326a1c5b5d76f1699a3
NAVEENKUMAR S <80678046+NaveenKumar-n4v@users.noreply.github.com>
2022-03-05 21:55:37 +0530

    Time: 15 ms (50.00%), Space: 12.9 MB (8.33%) - LeetHub

--- 2190-most-frequent-number-following-key-in-an-array/2190-most-frequent-number-following-key-in-an-array.cpp
+++ 2190-most-frequent-number-following-key-in-an-array/2190-most-frequent-number-following-key-in-an-array.cpp
@@ -1,24 +1,16 @@
 class Solution {
 public:
-    int mostFrequent(vector<int>& nums, int key) {
-        int n= nums.size();
-        map<int,int>freq;
-        for(int i=0;i<n-1;i++)
-        {
-            if(nums[i]==key){
-                freq[nums[i+1]]++;
-            }
+        int mostFrequent(vector<int>& nums, int key) {
+    vector<int> cnt(1001);
+    int n= size(nums) , mx=0, res=0;
+    for(int i=0;i<n-1;i++){
+        int x= nums[i+1];
+        if(nums[i]==key) {  //if we remove this statement then it would be answer to question -> MOST FREQUENT ELEMENT
+            cnt[x]++;
+            if(cnt[x]>mx) mx= cnt[x] , res= x;
         }
-        int mx=0;
-        int mi =0;
-        for(auto i : freq){
-            if(i.second>mx){
-                mx = i.second;
-                mi = i.first;
-                
-            }
-        }
-        return mi;
-       
     }
+    return res;
+}
+
 };
