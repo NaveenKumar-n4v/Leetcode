7ed8f1929e98e606e939fb686c835de809752f92
NAVEENKUMAR S <80678046+NaveenKumar-n4v@users.noreply.github.com>
2022-03-15 22:37:47 +0530

    Time: 0 ms (100.00%), Space: 7.2 MB (22.38%) - LeetHub

--- 876-middle-of-the-linked-list/876-middle-of-the-linked-list.cpp
+++ 876-middle-of-the-linked-list/876-middle-of-the-linked-list.cpp
@@ -14,7 +14,7 @@ public:
         
         ListNode* ptr1 = head;
         ListNode* ptr2 = head;
-        while(ptr2!= NULL && ptr2->next!=NULL){
+        while(ptr2!= NULL and ptr2->next!=NULL){
             ptr1=ptr1->next;
             ptr2=ptr2->next->next;
         }
