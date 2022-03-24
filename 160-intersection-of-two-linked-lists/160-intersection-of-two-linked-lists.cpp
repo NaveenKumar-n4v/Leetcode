a1e07c44f4c64a1021848150286213444a160958
NAVEENKUMAR S <80678046+NaveenKumar-n4v@users.noreply.github.com>
2022-03-24 22:31:50 +0530

    Time: 60 ms (56.58%), Space: 14.6 MB (73.44%) - LeetHub

--- 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.cpp
+++ 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.cpp
@@ -24,36 +24,55 @@ public:
         // return NULL;
         
         
-        ListNode* dummy1=h1;
-        ListNode* dummy2=h2;
-        int cnt1=0;
-        int cnt2=0;
-        while(dummy1 != NULL){
-            cnt1 +=1;
-            dummy1 = dummy1->next;
+        // OPTIMAL 1  
+        // O(N)+ O(M)+ O(N-M)+ O(M)
+        // O(2M)
+        // O(1)
+        // ListNode* dummy1=h1;
+        // ListNode* dummy2=h2;
+        // int cnt1=0;
+        // int cnt2=0;
+        // while(dummy1 != NULL){
+        //     cnt1 +=1;
+        //     dummy1 = dummy1->next;
+        // }
+        // while(dummy2 != NULL){
+        //     cnt2 +=1;
+        //     dummy2 = dummy2->next;
+        // }
+        // if(cnt1>=cnt2){
+        //     cnt1 = abs(cnt1-cnt2);
+        //     while(cnt1--){
+        //         h1 = h1->next;
+        //     }
+        // }
+        // else{
+        //     cnt2 = abs(cnt2-cnt1);
+        //     while(cnt2--){
+        //         h2= h2->next;
+        //     }
+        // }
+        // while(h1 && h2){
+        //     if(h1==h2)return h2;
+        //     h1 = h1->next;
+        //     h2 = h2->next;
+        // }
+        // return NULL;
+        // return h1;
+    
+        
+        
+        //Optimal 2
+        //O(2M)
+        //O(1)
+        if(!h1 || !h2)return NULL;
+        ListNode* dummy1 = h1;
+        ListNode* dummy2 = h2;
+        while(dummy1 != dummy2){
+            dummy1  = dummy1 == NULL?h2:dummy1->next;
+            dummy2 = dummy2 == NULL?h1:dummy2->next;
         }
-        while(dummy2 != NULL){
-            cnt2 +=1;
-            dummy2 = dummy2->next;
-        }
-        if(cnt1>=cnt2){
-            cnt1 = abs(cnt1-cnt2);
-            while(cnt1--){
-                h1 = h1->next;
-            }
-        }
-        else{
-            cnt2 = abs(cnt2-cnt1);
-            while(cnt2--){
-                h2= h2->next;
-            }
-        }
-        while(h1 && h2){
-            if(h1==h2)return h2;
-            h1 = h1->next;
-            h2 = h2->next;
-        }
-        return NULL;
-        return h1;
+        return dummy1;
+        
     }
 };
