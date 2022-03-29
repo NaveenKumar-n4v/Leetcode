d1d642991ceb7c980723e1cb3f37b634bab33cb7
NAVEENKUMAR S <80678046+NaveenKumar-n4v@users.noreply.github.com>
2022-03-29 20:27:44 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (95.51%) - LeetHub

--- 69-sqrtx/69-sqrtx.cpp
+++ 69-sqrtx/69-sqrtx.cpp
@@ -1,11 +1,19 @@
+#define ll long long
 class Solution {
 public:
     int mySqrt(int x) {
-        long long r = x;
-        while(r*r>x){
-            r = (r+x/r)/2;
+        ll low = 1;
+        ll high = x;
+        while(low <=high){
+            ll mid = low + (high - low)/2;
+            if(mid * mid == x)return mid;
+            else if(mid*mid>x){
+                high = mid-1;
+            }
+            else
+                low = mid+1;
         }
-        return r;
+        return high;
         
     }
 };
