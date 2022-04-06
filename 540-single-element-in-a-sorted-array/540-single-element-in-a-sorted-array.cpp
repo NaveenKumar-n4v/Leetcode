4d0a575a1f2074533d9ae1fabef6df7ed45a9e79
NAVEENKUMAR S <80678046+NaveenKumar-n4v@users.noreply.github.com>
2022-04-07 00:01:15 +0530

    Time: 39 ms (40.73%), Space: 22.3 MB (91.75%) - LeetHub

--- 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.cpp
+++ 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.cpp
@@ -5,15 +5,21 @@ public:
         
         int low =0;
         int n = nums.size();
+        // coz if single element in last index we need a index for low to occupy
         int high = n-2;
+        
         while(low <= high){
             int mid = low + (high - low)/2;
+            // mid^1 
+            // is mid is odd mid^1 will give the left even index 
+            // if mid id even mid^1 will give the right odd index
              if(nums[mid] == nums[mid^1]){
                  low = mid+1;
              }
             else
                 high = mid-1;
         }
+        // coz always low will cross the boundary
         return nums[low];
         
     }
