31550b8012d31a279654f2cda5efedf49cc83b9c
NAVEENKUMAR S <80678046+NaveenKumar-n4v@users.noreply.github.com>
2022-06-07 10:59:43 +0530

    Time: 8 ms (38.21%), Space: 7.5 MB (89.64%) - LeetHub

--- 90-subsets-ii/90-subsets-ii.cpp
+++ 90-subsets-ii/90-subsets-ii.cpp
@@ -1,25 +1,49 @@
+// class Solution {
+// private:
+//     void funz(int idx, vector<int>&arr, vector<int>&ds, set<vector<int>>&ans){
+//         if(idx == arr.size()){
+//             ans.insert(ds);
+//             return;
+//         }
+//         //pick
+//         ds.push_back(arr[idx]);
+//         funz(idx+1,arr,ds,ans);
+//         ds.pop_back();
+        
+//         funz(idx+1,arr,ds,ans);
+//     }
+// public:
+//     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+//         sort(nums.begin(),nums.end());
+//         set<vector<int>>ans;
+//         vector<int>ds;
+//         funz(0,nums,ds,ans);
+//         vector<vector<int>>v(ans.begin(),ans.end());
+//         return v;
+        
+//     }
+// };
+
+
 class Solution {
 private:
-    void funz(int idx, vector<int>&arr, vector<int>&ds, set<vector<int>>&ans){
-        if(idx == arr.size()){
-            ans.insert(ds);
-            return;
+    void helper(int idx, vector<vector<int>>&ans, vector<int>&ds, vector<int>&arr){
+        ans.push_back(ds);
+        for(int i= idx;i<arr.size();i++){
+            if(i>idx && arr[i]==arr[i-1]){
+                continue;
+            }
+            ds.push_back(arr[i]);
+            helper(i+1, ans, ds, arr);
+            ds.pop_back();
         }
-        //pick
-        ds.push_back(arr[idx]);
-        funz(idx+1,arr,ds,ans);
-        ds.pop_back();
-        
-        funz(idx+1,arr,ds,ans);
     }
 public:
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
         sort(nums.begin(),nums.end());
-        set<vector<int>>ans;
+        vector<vector<int>>ans;
         vector<int>ds;
-        funz(0,nums,ds,ans);
-        vector<vector<int>>v(ans.begin(),ans.end());
-        return v;
-        
+        helper(0, ans, ds, nums);
+        return ans;
     }
 };
