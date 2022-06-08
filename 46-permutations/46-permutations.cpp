dbe8d0d71d815a9b89440fbd148b2639c15be56c
NAVEENKUMAR S <80678046+NaveenKumar-n4v@users.noreply.github.com>
2022-06-09 01:37:21 +0530

    Time: 0 ms (100.00%), Space: 7.5 MB (98.59%) - LeetHub

--- 46-permutations/46-permutations.cpp
+++ 46-permutations/46-permutations.cpp
@@ -1,28 +1,49 @@
+// class Solution {
+// private:
+//     void helper( vector<int>&arr, vector<vector<int>>&ans, vector<int>&ds, map<int,int>&freq){
+//         if(arr.size()==ds.size()){
+//             ans.push_back(ds);
+//             return;
+//         }
+//         for(int i=0;i<arr.size();i++){
+//             if(freq[i] == 0){
+//                 ds.push_back(arr[i]);
+//                 freq[i]=1;
+//                 helper(arr, ans, ds, freq);
+//                 freq[i]=0;
+//                 ds.pop_back();
+//             }
+//         }
+        
+//     }
+// public:
+//     vector<vector<int>> permute(vector<int>& nums) {
+//         vector<vector<int>>ans;
+//         vector<int>ds;
+//         map<int,int>freq;
+//         for(int i=0;i<nums.size();i++)freq[i]=0;
+//         helper(nums, ans, ds, freq);
+//         return ans;
+//     }
+// };
+
 class Solution {
 private:
-    void helper( vector<int>&arr, vector<vector<int>>&ans, vector<int>&ds, map<int,int>&freq){
-        if(arr.size()==ds.size()){
-            ans.push_back(ds);
+    void helper(int idx, vector<vector<int>>&ans, vector<int>&arr){
+        if(idx == arr.size()){
+            ans.push_back(arr);
             return;
         }
-        for(int i=0;i<arr.size();i++){
-            if(freq[i] == 0){
-                ds.push_back(arr[i]);
-                freq[i]=1;
-                helper(arr, ans, ds, freq);
-                freq[i]=0;
-                ds.pop_back();
-            }
+        for(int i = idx;i<arr.size();i++){
+            swap(arr[idx],arr[i]);
+            helper(idx+1, ans, arr);
+            swap(arr[idx],arr[i]);
         }
-        
     }
 public:
     vector<vector<int>> permute(vector<int>& nums) {
         vector<vector<int>>ans;
-        vector<int>ds;
-        map<int,int>freq;
-        for(int i=0;i<nums.size();i++)freq[i]=0;
-        helper(nums, ans, ds, freq);
+        helper(0, ans, nums);
         return ans;
     }
 };
