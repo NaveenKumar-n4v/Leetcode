12336ab1ed1b2d34fb942e7034864abea789fb23
NAVEENKUMAR S <80678046+NaveenKumar-n4v@users.noreply.github.com>
2022-06-10 21:58:43 +0530

    Time: 172 ms (30.53%), Space: 67.8 MB (54.40%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.cpp
+++ 53-maximum-subarray/53-maximum-subarray.cpp
@@ -6,7 +6,7 @@ public:
         int maxi = INT_MIN;
         for(auto i : nums){
             sum +=i;
-            if(sum >=maxi)maxi = sum;
+            if(sum >maxi)maxi = sum;
             if(sum < 0 )sum =0;
         }
         return maxi;
