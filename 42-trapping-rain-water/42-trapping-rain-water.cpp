d7d7ea3c2fc5db5a0f17069019bbe66f09ed2936
NAVEENKUMAR S <80678046+NaveenKumar-n4v@users.noreply.github.com>
2022-06-15 00:50:47 +0530

    Time: 18 ms (66.47%), Space: 19.8 MB (44.23%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.cpp
+++ 42-trapping-rain-water/42-trapping-rain-water.cpp
@@ -38,34 +38,67 @@ public:
         /*
         O(3N)
         */
-        int n =height.size();
-        vector<int>prefix(n);
-        vector<int>suffix(n);
-        int counter =0;
+//         int n =height.size();
+//         vector<int>prefix(n);
+//         vector<int>suffix(n);
+//         int counter =0;
         
-        for(int i = 0;i<n;i++){
+//         for(int i = 0;i<n;i++){
             
-            if(i == 0){
-                prefix[i] = height[i];
-            }
-            else
-                prefix[i] = max(prefix[i-1], height[i]);
-        }
-        for(int i = n-1; i>=0; i--){
-            if(i == n-1){
-                suffix[i] = height[i]; 
-            }
-            else
-                suffix[i] = max(suffix[i+1], height[i]);
-        }
+//             if(i == 0){
+//                 prefix[i] = height[i];
+//             }
+//             else
+//                 prefix[i] = max(prefix[i-1], height[i]);
+//         }
+//         for(int i = n-1; i>=0; i--){
+//             if(i == n-1){
+//                 suffix[i] = height[i]; 
+//             }
+//             else
+//                 suffix[i] = max(suffix[i+1], height[i]);
+//         }
         
-        for(int i = 0;i<n;i++){
+//         for(int i = 0;i<n;i++){
             
-            int val = min(prefix[i],suffix[i]);
-            counter += val - height[i];
+//             int val = min(prefix[i],suffix[i]);
+//             counter += val - height[i];
+            
+//         }
+//         return counter;
+        
+        
+        
+        /*
+        O(N) 2Pointer
+        O(1)
+        */
+        int n = height.size();
+        int left = 0;
+        int right = n-1;
+        int leftMaxi = 0;
+        int rightMaxi = 0;
+        int counter = 0;
+        
+        while(left <= right){
+            
+            if(height[left] <= height[right]){
+                
+                if(height[left] >= leftMaxi)leftMaxi = height[left];
+                else
+                    counter += abs(height[left] - leftMaxi);
+                left++;
+            }
+            else{
+                if(height[right] >= rightMaxi)rightMaxi = height[right];
+                else
+                    counter += abs(height[right]-rightMaxi);
+                right--;
+            }
             
         }
         return counter;
+        
         
         
     }
