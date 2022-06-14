944cb311a41453fb0c0868da5572e19eccba72c7
NAVEENKUMAR S <80678046+NaveenKumar-n4v@users.noreply.github.com>
2022-06-15 01:16:39 +0530

    Time: 19 ms (31.95%), Space: 7.4 MB (8.30%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.cpp
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -1,9 +1,20 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
-        if(s!=t)return false;
+        
+        if(s.size() != t.size())return false;
+        map<char,int>mpp;
+        map<char,int>mpp2;
+        bool flag = true;
+        for(auto it : s)mpp[it]++;
+        for(auto it : t)mpp2[it]++;
+        for (auto i = mpp.begin(),it = mpp2.begin(); i != mpp.end(), it != mpp2.end(); i++,it++){
+            if((i->first == it->first) && (i->second == it->second)){
+                continue;
+            }
+            else
+                return false;
+        }
         return true;
         
     }
