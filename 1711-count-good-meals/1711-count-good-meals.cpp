d8c2a5e3331e912129dec2fc89c2d96509a1bed1
NAVEENKUMAR S <80678046+NaveenKumar-n4v@users.noreply.github.com>
2022-06-19 01:35:49 +0530

    Time: 895 ms (29.09%), Space: 58.1 MB (48.55%) - LeetHub

---
+++ 1711-count-good-meals/1711-count-good-meals.cpp
@@ -0,0 +1,22 @@
+
+class Solution {
+public:
+    int countPairs(vector<int>& v) {
+        int mod = 1000000007;
+        if(v.size() == 1) return 0;
+        unordered_map<int,int>mpp;
+        sort(v.begin(),v.end());
+        mpp[v[0]]++;
+        int counter =0;
+        for(int i = 1;i<v.size();i++){
+            for(int j = 0;j<22;j++){
+                if(mpp.find(pow(2,j)-v[i])!= mpp.end()){
+                    counter+=mpp[pow(2,j) - v[i]]%mod;
+                    counter %= mod;
+                }
+            }
+            mpp[v[i]]++;
+        }
+    return counter %(int)mod;
+    }
+};
