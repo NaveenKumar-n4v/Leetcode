e06583b9e97361ccd55b1fac00d6269ec3e97ab9
NAVEENKUMAR S <80678046+NaveenKumar-n4v@users.noreply.github.com>
2022-07-23 23:34:24 +0530

    Time: 0 ms (100.00%), Space: 8.5 MB (36.45%) - LeetHub

--- 145-binary-tree-postorder-traversal/145-binary-tree-postorder-traversal.cpp
+++ 145-binary-tree-postorder-traversal/145-binary-tree-postorder-traversal.cpp
@@ -43,34 +43,65 @@ public:
         // return ans;
         
         
-        //iterative version - 1 stack
-        vector<int>ans;
-        stack<TreeNode*>s1;
+        // //iterative version - 1 stack
+        // vector<int>ans;
+        // stack<TreeNode*>s1;
+        // TreeNode* node = root;
+        // TreeNode* temp = root;
+        // while(!s1.empty() || node != NULL){
+        //     //go to extreme left and push to single stack
+        //     if(node != NULL){
+        //         s1.push(node);
+        //         node = node->left;
+        //     }
+        //     else{
+        //         temp = s1.top()->right;
+        //         if(temp == NULL){
+        //             temp = s1.top();
+        //             s1.pop();
+        //             ans.push_back(temp->val);
+        //             while(!s1.empty() && temp == s1.top()->right ){
+        //                 temp = s1.top();
+        //                 s1.pop();
+        //                 ans.push_back(temp->val);
+        //             }
+        //         }
+        //         else
+        //             node = temp;
+        //     }
+        // }
+        // return ans;
+        
+        
+        //Code for all traversals in one stack
+        stack<pair<TreeNode*,int>>st;
+        vector<int>preorder;
+        vector<int>inorder;
+        vector<int>postorder;
         TreeNode* node = root;
-        TreeNode* temp = root;
-        while(!s1.empty() || node != NULL){
-            //go to extreme left and push to single stack
-            if(node != NULL){
-                s1.push(node);
-                node = node->left;
+        st.push({root, 1});
+        if(node == NULL)return preorder;
+        while(!st.empty()){
+            auto it = st.top();
+            st.pop();
+            if(it.second == 1){
+                preorder.push_back(it.first->val);
+                it.second++;
+                st.push(it);
+                if(it.first->left != NULL)st.push({it.first->left, 1});
+            }
+            
+            else if(it.second == 2){
+                inorder.push_back(it.first->val);
+                it.second++;
+                st.push(it);
+                if(it.first->right != NULL)st.push({it.first->right, 1});
             }
             else{
-                temp = s1.top()->right;
-                if(temp == NULL){
-                    temp = s1.top();
-                    s1.pop();
-                    ans.push_back(temp->val);
-                    while(!s1.empty() && temp == s1.top()->right ){
-                        temp = s1.top();
-                        s1.pop();
-                        ans.push_back(temp->val);
-                    }
-                }
-                else
-                    node = temp;
+                postorder.push_back(it.first->val);
             }
         }
-        return ans;
+        return postorder;
         
     }
 };
