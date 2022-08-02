94bb7e255dd75c6c5829b2c4c5d543af531daf97
NAVEENKUMAR S <80678046+NaveenKumar-n4v@users.noreply.github.com>
2022-08-02 21:29:38 +0530

    Time: 36 ms (89.98%), Space: 30.9 MB (20.99%) - LeetHub

--- 222-count-complete-tree-nodes/222-count-complete-tree-nodes.cpp
+++ 222-count-complete-tree-nodes/222-count-complete-tree-nodes.cpp
@@ -10,18 +10,30 @@
  * };
  */
 class Solution {
+private:
+    void func(TreeNode* node, int &cnt){
+        if(node == NULL)return;
+        cnt++;
+        func(node->left, cnt);
+        func(node->right, cnt);
+    }
 public:
     int countNodes(TreeNode* root) {
         
+//         if(root == NULL)return 0;
+        
+//         int l = lh(root);
+//         int r = rh(root);
+        
+//         // on complete BT
+//         if( l == r )return pow(2, l) - 1;
+        
+//         return 1 + countNodes(root->left) + countNodes(root->right); 
+        
+        int cnt =0;
         if(root == NULL)return 0;
-        
-        int l = lh(root);
-        int r = rh(root);
-        
-        // on complete BT
-        if( l == r )return pow(2, l) - 1;
-        
-        return 1 + countNodes(root->left) + countNodes(root->right); 
+        func(root, cnt);
+        return cnt;
         
     }
     int lh(TreeNode* root){
