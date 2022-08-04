3a34dabfc499b764947b06a53cbc91cb1b050bcb
NAVEENKUMAR S <80678046+NaveenKumar-n4v@users.noreply.github.com>
2022-08-04 10:17:43 +0530

    Time: 35 ms (72.40%), Space: 22.3 MB (55.64%) - LeetHub

--- 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.cpp
+++ 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.cpp
@@ -1,26 +1,8 @@
 class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
-        
-        
-        int low =0;
-        int n = nums.size();
-        // coz if single element in last index we need a index for low to occupy
-        int high = n-2;
-        
-        while(low <= high){
-            int mid = low + (high - low)/2;
-            // mid^1 
-            // is mid is odd mid^1 will give the left even index 
-            // if mid id even mid^1 will give the right odd index
-             if(nums[mid] == nums[mid^1]){
-                 low = mid+1;
-             }
-            else
-                high = mid-1;
-        }
-        // coz always low will cross the boundary
-        return nums[low];
-        
+        int val =0;
+        for(auto i : nums)val=val^i;
+        return val;
     }
 };
