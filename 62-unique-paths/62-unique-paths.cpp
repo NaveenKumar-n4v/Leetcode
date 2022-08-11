452b01787d3539035f037e5c7344a31910ad4ae4
NAVEENKUMAR S <80678046+NaveenKumar-n4v@users.noreply.github.com>
2022-08-12 00:05:57 +0530

    Time: 6 ms (12.59%), Space: 6 MB (75.12%) - LeetHub

---
+++ 62-unique-paths/62-unique-paths.cpp
@@ -0,0 +1,20 @@
+class Solution {
+private:
+//     int func(int i, int j, int m, int n){
+//         if(i == n-1 && j == m-1)return 1;
+//         if(i >= n || j >= m )return 0;
+//         return func(i+1, j, m, n) + func(i, j+1, m, n);
+        
+//     }
+public:
+    int uniquePaths(int m, int n) {
+        // return func(0, 0, m, n);
+        int N = m + n - 2;
+        int r = m - 1;
+        double ans  = 1;
+        for(int i = 1;i<= r;i++){
+            ans = ans * ( N - r + i ) / i; 
+        }
+        return (int)ans;
+    }
+};
