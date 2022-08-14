861c96a94af1eac4a40b1c76f4d2ceebd82f6fd5
NAVEENKUMAR S <80678046+NaveenKumar-n4v@users.noreply.github.com>
2022-08-14 21:03:33 +0530

    Time: 40 ms (69.28%), Space: 23.2 MB (63.86%) - LeetHub

--- 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.cpp
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.cpp
@@ -15,14 +15,20 @@ public:
         
         if(root == NULL)return NULL;
         
+        // find value in root
         int x = root->val;
         
+        // if both p and q is smaller go to left subtree
         if(x > p->val && x > q->val){
             return lowestCommonAncestor(root->left, p, q);
         }
+        
+        // if both p and q is greater go to right subtree
         if(x < p->val && x < q->val){
             return lowestCommonAncestor(root->right, p, q);
         }
+        
+        // one small or one greater then thats the last point of intersection and return the intersion
         return root;
     }
 };
