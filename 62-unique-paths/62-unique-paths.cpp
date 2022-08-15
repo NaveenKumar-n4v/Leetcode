d79b33714323d2e0013e7b4a158deac9118fed59
NAVEENKUMAR S <80678046+NaveenKumar-n4v@users.noreply.github.com>
2022-08-15 10:35:54 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (46.25%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -1,20 +1,23 @@
 class Solution {
 private:
-//     int func(int i, int j, int m, int n){
-//         if(i == n-1 && j == m-1)return 1;
-//         if(i >= n || j >= m )return 0;
-//         return func(i+1, j, m, n) + func(i, j+1, m, n);
+    int func(int i, int j, int m, int n, vector<vector<int>>&dp){
+        if(i == m-1 && j == n-1)return 1;
+        if(i >= m || j >= n )return 0;
+        if(dp[i][j] != -1)return dp[i][j];
+        return dp[i][j] = func(i+1, j, m, n, dp) + func(i, j+1, m, n, dp);
         
-//     }
+    }
 public:
     int uniquePaths(int m, int n) {
-        // return func(0, 0, m, n);
-        int N = m + n - 2;
-        int r = n - 1;
-        long long ans  = 1;
-        for(int i = 1;i<= n-1;i++){
-            ans = ans * ( N - r + i ) / i; 
-        }
-        return ans;
+        vector<vector<int>>dp(m, vector<int>(n, -1));
+        return func(0, 0, m, n, dp);
+        
+        // int N = m + n - 2;
+        // int r = n - 1;
+        // long long ans  = 1;
+        // for(int i = 1;i<= n-1;i++){
+        //     ans = ans * ( N - r + i ) / i; 
+        // }
+        // return ans;
     }
 };
