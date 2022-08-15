a6c75872f2c530fdc75f3f952bcd599dc55b752b
NAVEENKUMAR S <80678046+NaveenKumar-n4v@users.noreply.github.com>
2022-08-16 01:39:56 +0530

    Time: 154 ms (30.40%), Space: 13.1 MB (88.03%) - LeetHub

--- 23-merge-k-sorted-lists/23-merge-k-sorted-lists.cpp
+++ 23-merge-k-sorted-lists/23-merge-k-sorted-lists.cpp
@@ -10,30 +10,30 @@
  */
 class Solution {
 public:
-    ListNode* merge2Lists(ListNode* list1,ListNode* list2)
-    {
-        ListNode* list3 =new ListNode();
-        ListNode* head=list3;
-        ListNode* temp=list1;
-        while(list1!=NULL and list2!=NULL)
-        {
-            if(list1->val<list2->val)
-            {
-                list3->next=list1;
-                list1=list1->next;
+   ListNode* merge2Lists(ListNode* l1, ListNode* l2) {
+        
+        //example 
+        //      5 7 9
+        //    3 4 8 10
+        if(l1==NULL)return l2;
+        else if(l2==NULL) return l1;
+        
+        if(l1->val > l2->val)swap(l1,l2);
+        //final to ret
+        ListNode* res = l1;
+        
+        
+        while(l1!=NULL and l2!=NULL){
+            //temp is used coz l1 reaches the next ptr and we have to break link in previous ptr
+            ListNode* temp = NULL;
+            while(l1 != NULL and l1->val <= l2->val){
+                temp = l1;
+                l1=l1->next;
             }
-            else
-            {
-                list3->next=list2;
-                list2=list2->next;
-            }
-            list3=list3->next;
+            temp->next = l2;
+            swap(l1,l2);
         }
-        if(list1!=NULL)
-            list3->next=list1;
-        else if(list2!=NULL)
-            list3->next=list2;
-        return head->next;
+        return res;
     }
     ListNode* mergeKLists(vector<ListNode*>& lists) {
         ListNode* ans=NULL;
