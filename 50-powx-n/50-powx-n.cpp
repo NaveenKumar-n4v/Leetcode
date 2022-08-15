ce3df1654f9e8e862254e8ee948f7474ea42c47b
NAVEENKUMAR S <80678046+NaveenKumar-n4v@users.noreply.github.com>
2022-08-15 22:27:00 +0530

    Time: 3 ms (47.73%), Space: 5.9 MB (80.61%) - LeetHub

--- 50-powx-n/50-powx-n.cpp
+++ 50-powx-n/50-powx-n.cpp
@@ -1,7 +1,23 @@
 class Solution {
 public:
     double myPow(double x, int n) {
-        return pow(x,n);
         
+        double ans = 1.0;
+        long long copy = n;
+        if(copy < 0)copy = -1 * copy;
+        while(copy){
+            if(copy % 2){
+                ans = ans * x;
+                copy = copy -1;
+            }
+            else{
+                x = x * x;
+                copy = copy / 2;
+            }
+        }
+        if(n < 0){
+            ans = (double) (1.0) / (double)(ans);
+        }
+        return ans;
     }
 };
